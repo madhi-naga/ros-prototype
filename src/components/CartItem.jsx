42af8be9111dae46574f18be2202c58ec7019aef
Madhi <madhi.naga@gmail.com>
2022-11-18 19:01:56 -0500

    Updated Cart to include selected items

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
@@ -6,36 +6,66 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./item.css";
 
-export class CartItem extends Component {
-  render() {
-    return (
-      <div className="cart-item" style={itemStyle}>
-        <div className="row">
-          <div className="col-md-4">
-            <img className="item-img" src="https://foodess.com/wp-content/uploads/2022/10/Foodess-Best-Butter-Chicken-1-2.jpg" alt="" />
-          </div>
-          <div className="col-md-4">
-            <h4>Item Name</h4>
-            <p>Description</p>
-          </div>
-          <div className="col-md-2">
-            <IconButton color="primary" aria-label="add to shopping cart">
-              <AddIcon />
-            </IconButton>
-            0
-            <IconButton color="primary" aria-label="add to shopping cart">
-              <RemoveIcon />
-            </IconButton>
-          </div>
-          <div className="col-md-2">
-            <h6>Due:</h6> $10.00
-          </div>
-        </div>
-        <hr class='rounded'/>
-      </div>
-    );
+
+const CartItem = ({cartItems, setCartItems, itemName, item}) => {
+  const [quantity, setQuantity] = useState(item.quantity);
+  const [price, setPrice] = useState(item.quantity * item.price);
+  
+  const updateCart = (onClick) => {
+    onClick();
+    console.log(quantity);
+    item.quantity = quantity;
+    cartItems[itemName] = item;
+    setCartItems(cartItems);
+    console.log(cartItems[itemName])
+    console.log(price);
   }
+  
+  const onIncrease = () => {
+    setQuantity(quantity+1);
+    console.log("oninc", quantity);
+    setPrice(price + item.price);
+  }
+  const onDecrease = () => {
+    if (quantity > 1) {
+      setQuantity(quantity-1);
+      setPrice(price - item.price);
+    }
+  }
+  useEffect(() => {
+    item.quantity = quantity;
+    cartItems[itemName] = item;
+    setCartItems(cartItems);
+    console.log(cartItems[itemName]);
+  });
+
+  return (
+    <div className="cart-item" style={itemStyle}>
+      <div className="row">
+        <div className="col-md-4">
+          <img className="item-img" src={item.img} alt="" />
+        </div>
+        <div className="col-md-4">
+          <h5>{itemName}</h5>
+        </div>
+        <div className="col-md-2">
+          <IconButton onClick={onIncrease} color="primary" aria-label="add quantity">
+            <AddIcon on/>
+          </IconButton>
+          {quantity}
+          <IconButton disabled={quantity <= 1} onClick={onDecrease} color="primary" aria-label="reduce quantity">
+            <RemoveIcon />
+          </IconButton>
+        </div>
+        <div className="col-md-2">
+          <h6>Due:</h6> ${price}
+        </div>
+      </div>
+      <hr class='rounded'/>
+    </div>
+  );
 }
+
 
 const itemStyle = {
     borderColor:"black",
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import {
   AppBar,
   Typography,
@@ -7,93 +8,119 @@ import {
   Box,
   Toolbar,
   TextField,
-} from "@mui/material";
-import Button from "@mui/material/Button";
-import { CartItem } from "../components/CartItem";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import ArrowBackIcon from "@mui/icons-material/ArrowBackRounded";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./cart.css";
-export class Cart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: null,
-    };
-  }
+} from '@mui/material';
+import Button from '@mui/material/Button';
+import CartItem from '../components/CartItem';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import ArrowBackIcon from '@mui/icons-material/ArrowBackRounded';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './cart.css';
 
-  render() {
-    return (
-      <div>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-              >
-                <ArrowBackIcon />
-              </IconButton>
-              <Typography
-                variant="h4"
-                component="div"
-                sx={{
-                  flexGrow: 1,
-                  textAlign: "center",
-                  padding: "5px",
-                  fontWeight: 600,
-                }}
-              >
-                Shopping Cart
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Container sx={{padding:"20px"}}>
-            <h3>Cart Summary:</h3>
-            <div className="container" style={containerStyle}>
-              {[...Array(5)].map((x, i) => (
-                <CartItem />
-              ))}
-            </div>
-          </Container>
-        </Box>
-        <Container sx={{padding:"20px"}}>
-          <div className="requirements">
-            <h3>Requirements:</h3>
-            <TextField
-              id="outlined-textarea"
-              label=""
-              placeholder="Type here to send a special note to the chefs..."
-              rows={2}
-              multiline
-              fullWidth
-            />
+const Cart = ({ cartItems, setCartItems }) => {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  console.log(cartItems);
+  const itemNames = () => cartItems ? Object.keys(cartItems) : [];
+  console.log(itemNames());
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={() => {
+								navigate("/Menu");
+							}}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{
+                flexGrow: 1,
+                textAlign: 'center',
+                padding: '5px',
+                fontWeight: 600,
+              }}
+            >
+              Shopping Cart
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container sx={{ padding: '20px' }}>
+          <h3>Cart Summary:</h3>
+          <div className="container" style={containerStyle}>
+            {itemNames().map((name) => (
+              <CartItem cartItems={cartItems} setCartItems={setCartItems} itemName={name} item={cartItems[name]} />
+            ))}
           </div>
         </Container>
-        <Container sx={{padding:"20px"}}>
-          <div className="payment">
-            <h3>Amount Due:</h3>
-            <Button>Submit & Pay Now</Button>
-            <Button>Cancel</Button>
-          </div>
-        </Container>
-      </div>
-    );
-  }
-}
+      </Box>
+      <Container sx={{ padding: '20px' }}>
+        <div className="requirements">
+          <h3>Requirements:</h3>
+          <TextField
+            id="outlined-textarea"
+            label=""
+            placeholder="Type here to send a special note to the chefs..."
+            rows={2}
+            multiline
+            fullWidth
+          />
+        </div>
+      </Container>
+      <Container sx={{ padding: '20px' }}>
+        <div className="payment">
+          <h3>Amount Due:</h3>
+          <Button
+            onClick={() => {
+              this.handleClick();
+            }}
+          >
+            Submit & Pay Now
+          </Button>
+          <Button>Cancel</Button>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+// export class Cart extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       cartItems: this.props.location,
+//     };
+//   }
+
+//   handleClick(){
+//     console.log("Cart:", this.state.cartItems);
+//     console.log("Cart2:", this.props.location.state);
+//   }
+
+//   render() {
+
+//   }
+// }
 
 const containerStyle = {
-  backgroundColor: "#F2F2F2",
-  margin: "20px",
-  paddingTop: "20px",
-  borderRadius: "10px",
-  borderStyle: "grooved",
-  borderStyle: "solid",
+  backgroundColor: '#FAFAFA',
+  marginTop: '20px',
+  paddingTop: '20px',
+  borderRadius: '10px',
+  borderStyle: 'grooved',
+  borderStyle: 'solid',
 };
 
 export default Cart;
