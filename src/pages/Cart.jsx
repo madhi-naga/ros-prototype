2c76494b52882ab0bb82531d8e979fa447233f2b
Madhi <madhi.naga@gmail.com>
2022-11-19 17:15:09 -0500

    Added submit popup in cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,6 +9,11 @@ import {
   Toolbar,
   TextField,
 } from '@mui/material';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 import Button from '@mui/material/Button';
 import CartItem from '../components/CartItem';
 import ArrowBackIcon from '@mui/icons-material/ArrowBackRounded';
@@ -16,18 +21,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './cart.css';
 
-const formatter = new Intl.NumberFormat("en-US", {
-	style: "currency",
-	currency: "CAD",
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'CAD',
 
-	// These options are needed to round to whole numbers if that's what you want.
-	//minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-	//maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
 
 const Cart = ({ cartItems, setCartItems }) => {
-  
-  const itemNames = () => cartItems ? Object.keys(cartItems) : [];
+  const itemNames = () => (cartItems ? Object.keys(cartItems) : []);
   const [quantities, setQuantities] = useState([]);
 
   const sumDue = () => {
@@ -37,7 +41,7 @@ const Cart = ({ cartItems, setCartItems }) => {
       return currItem.quantity * currItem.price;
     });
     return prices.reduce((a, b) => a + b, 0);
-  }
+  };
 
   const navigate = useNavigate();
   const [totalDue, setTotalDue] = useState(0);
@@ -46,6 +50,24 @@ const Cart = ({ cartItems, setCartItems }) => {
     const sumVal = sumDue();
     setTotalDue(sumVal);
   }, [cartItems, quantities]);
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleFeedback = () => {
+    navigate('/Feedback');
+  };
+
+  const handleMenu = () => {
+    navigate('/Menu');
+  };
 
   return (
     <div>
@@ -58,9 +80,7 @@ const Cart = ({ cartItems, setCartItems }) => {
               color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
-              onClick={() => {
-								navigate("/Menu");
-							}}
+              onClick={handleMenu}
             >
               <ArrowBackIcon />
             </IconButton>
@@ -82,7 +102,14 @@ const Cart = ({ cartItems, setCartItems }) => {
           <h3>Cart Summary:</h3>
           <div className="container" style={containerStyle}>
             {itemNames().map((name) => (
-              <CartItem cartItems={cartItems} setCartItems={setCartItems} itemName={name} item={cartItems[name]} quantities={quantities} setQuantities={setQuantities} />
+              <CartItem
+                cartItems={cartItems}
+                setCartItems={setCartItems}
+                itemName={name}
+                item={cartItems[name]}
+                quantities={quantities}
+                setQuantities={setQuantities}
+              />
             ))}
           </div>
         </Container>
@@ -105,15 +132,44 @@ const Cart = ({ cartItems, setCartItems }) => {
           <h3>Amount Due:</h3>
           <h5>{formatter.format(totalDue)}</h5>
           <Button
-            onClick={() => {
-              this.handleClick();
+            onClick={handleClickOpen}
+            style={{
+              backgroundColor: '#148531',
+              marginRight:'20px'
             }}
+            variant="contained"
           >
             Submit & Pay Now
           </Button>
-          <Button>Cancel</Button>
+          <Button
+            onClick={handleMenu}
+            style={{
+              backgroundColor: '#D40734',
+            }}
+            variant="contained"
+          >
+            Cancel
+          </Button>
         </div>
       </Container>
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>Please Wait for Assistance</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Your order has been submitted! A server will meet you shortly to
+            complete your payment.
+          </DialogContentText>
+          <br />
+          <DialogContentText id="alert-dialog-description">
+            Done your meal? Please rate how we did today!
+          </DialogContentText>
+          <DialogActions>
+            <Button onClick={handleFeedback} autoFocus>
+              Proceed to Feedback
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
